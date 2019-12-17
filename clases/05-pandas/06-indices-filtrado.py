f72bbcb772424c320bc95e1f59a9c36ad8bb69c0
Leslie Díaz <lmdiaz36@gmail.com>
2019-12-17 09:04:07 -0500

    iloc-loc

--- clases/05-pandas/06-indices-filtrado.py
+++ clases/05-pandas/06-indices-filtrado.py
@@ -9,4 +9,18 @@ import pandas as pd
 
 path_guardado_bin = "C://Users//USRBET//Documents//py-diaz-yanangomez-leslie-mishell//clases//05-pandas//data//artwork_data_bin.pickle"
 dataframe = pd.read_pickle(path_guardado_bin)
-# df = dataframe.iloc[49999:50100,:].copy()
+
+## Obtener nombre artistas
+serie_artistas_repetidos = dataframe['artist']
+
+artistas = pd.unique(serie_artistas_repetidos)
+
+## Cuantos artistas hay?
+artistas.size # or len(artistas)
+
+## obras de blake, william
+blake = serie_artistas_repetidos == "Blake, William"
+blake.value_counts()
+serie_artistas_repetidos.value_counts()
+
+df_blake = dataframe[blake]
