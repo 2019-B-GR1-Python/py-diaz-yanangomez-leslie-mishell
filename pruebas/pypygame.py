2edcf602d96372ac3b57c7e40762ac1572cdc06a
Leslie Díaz <lmdiaz36@gmail.com>
2019-12-18 16:05:52 -0500

    fondo-arreglar-juego

--- pruebas/pypygame.py
+++ pruebas/pypygame.py
@@ -1,41 +1,51 @@
 import pygame
 
 pygame.init()
-screen = pygame.display.set_mode((400,300))
+screen = pygame.display.set_mode((1920,1200))
+clock = pygame.time.Clock()
+pygame.display.set_caption("OSU! Game")
+background_image = pygame.image.load("osu.jpeg").convert()
+
 done = False
 size = (50, 50)
 size_borde = (100,100)
-radius = 25
+radio = 25
 radio_borde = 50
-position = (100,100)
-position2 = (200,70)
-position3 = (250, 140)
-position4 = (300, 90)
+#CONSTANTES
+BORDE = 5
 
 # COLORES
 COLOR_CIRCULO = pygame.Color(71, 98, 79)
 COLOR_BORDE = pygame.Color(82, 173, 156)
+COLOR_FONDO = (0, 0, 0)
 # FIN COLORES
+
+
+def dibujar_circulo(tamanio, radio, is_guia = True):
+	circle = pygame.Surface(tamanio)	
+	if(is_guia):
+		pygame.draw.circle(circle, COLOR_BORDE, (radio, radio), radio, BORDE)
+	else:
+		pygame.draw.circle(circle, COLOR_CIRCULO, (radio, radio), radio)
+	return circle
+
+# CIRCULOS
+circle_filled = dibujar_circulo(size, radio, False)
+circle = dibujar_circulo(size_borde,radio_borde)
+# FIN CIRCULOS
 
 while not done:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			done = True
-	screen.blit(circle, (25, 25))		
-	screen.blit(circle_filled, size)
-	screen.blit(circle_filled, position)
-	screen.blit(circle_filled, position2)
-	screen.blit(circle_filled, position3)
-	screen.blit(circle_filled, position4)
-	pygame.display.update()
+	# Visible o no el cursor
+	screen.blit(background_image, [0, 0])
+	pygame.mouse.set_visible(True)
+	x, y = pygame.mouse.get_pos()
+	screen.blit(circle_filled, (x, y))
 	pygame.display.flip()
+	clock.tick(60)
 
-def dibujar_circulo(tamanio, radio, borde = 5):
-	circle = pygame.Surface(tamanio)	
-	if(borde):
-		pygame.draw.circle(circle, COLOR_BORDE, (radio, radio), radio, borde)
-	else:
-		pygame.draw.circle(circle, COLOR_CIRCULO, (radio, radio), radio)
-	return circle
+
 	
 	
